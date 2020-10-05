f5d846f2f5e89ccabbf3102c2797dc5dfd2191fb
soraya <soraya.astaghforellahi@mail.polimi.it>
2020-10-06 00:40:17 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,29 @@
 
+function preload(){
+  // put preload code here
+}
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
+  frameRate(24);
+  background("black");
+}
+function draw() {
+
+  noFill();
+
+ {
+  translate(width/2,height/2);
+    rotate(frameCount*3);
+    stroke("gold");
+    strokeWeight(1); stroke(lerpColor(color("magenta"),color("yellow"),frameCount/150));
+    square(300,80,20); square(20,25,20); square(50,25,20); square(80,10,20); square(110,10,200);
+    colorMode(HSB, 100);
+  stroke(100*mouseX/windowWidth, 100, 100);
+  ellipse(300,10,70);ellipse(300,100,100);
+  strokeWeight(1);
+  rect(-width/2, -height/2, width);
+  pop();
+
+}
+}
